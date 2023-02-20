fc4da39404008d5acc6e86211cb2b214f79bcd76
amine-benkhelfallah <amine.benkhelfallah@laplateforme.io>
2023-02-20 13:57:47 +0100

    Portfolio

---
+++ css/index.php
@@ -0,0 +1,22 @@
+
+<link rel="stylesheet" type="text/css" href="stylesheet.css">
+
+ <?php
+//include 'connectbd.php'
+?>
+ 
+
+<body>
+
+ <div class="wrapper">
+        <div class="cube-box">
+            <img src="img/img-1.jpg">
+            <img src="img/img-2.jpg">
+            <img src="img/img-3.jpg">
+            <img src="img/img-4.jpg">
+            <img src="img/img-5.jpg">
+            <img src="img/img-6.jpg">
+        </div>
+    </div>
+ 
+</body>
